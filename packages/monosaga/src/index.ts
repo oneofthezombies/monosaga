283981257256db98bfd00e905bda98415ea09829
oneofthezombies <hunhoekim@gmail.com>
2025-04-08 01:14:38 -0400

    wip

--- packages/monosaga/src/index.ts
+++ packages/monosaga/src/index.ts
@@ -3,150 +3,6 @@ import type { MaybePromise } from "@monosaga/utils";
 import "dotenv/config";
 import { randomUUID } from "node:crypto";
 
-export type SagaConfig = {
-  name: string;
-  steps: StepConfig[];
-};
-
-export type StepConfig =
-  | {
-      kind: "in-tx";
-      onExecute: (c: OnExecuteInTxContext) => MaybePromise<StepOutput>;
-      onCompensate: (c: OnCompensateInTxContext) => MaybePromise<StepOutput>;
-    }
-  | {
-      kind: "out-of-tx";
-      onExecute: (c: OnExecuteOutOfTxContext) => MaybePromise<StepOutput>;
-      onCompensate: (c: OnCompensateOutOfTxContext) => MaybePromise<StepOutput>;
-    };
-
-export type OnExecuteInTxContext = {
-  tx: pg.PoolClient;
-  data: Record<string, unknown>;
-  input: Record<string, unknown>;
-};
-
-export type OnCompensateInTxContext = {
-  tx: pg.PoolClient;
-  data: Record<string, unknown>;
-  output: Record<string, unknown>;
-};
-
-export type OnExecuteOutOfTxContext = {
-  data: Record<string, unknown>;
-  input: Record<string, unknown>;
-};
-
-export type OnCompensateOutOfTxContext = {
-  data: Record<string, unknown>;
-  output: Record<string, unknown>;
-};
-
-export type StepOutput = Record<string, unknown>;
-
-// test();
-
-// export async function test() {
-//   const saga: SagaConfig = {
-//     name: "create-user",
-//     steps: [
-//       {
-//         kind: "out-of-tx",
-//         async onExecute(c) {
-//           const emailEncrypted = Reflect.get(c.input, "emailEncrypted") as
-//             | string
-//             | undefined;
-//           if (!emailEncrypted) throw new Error();
-//           const passwordHash = Reflect.get(c.input, "passwordHash") as
-//             | string
-//             | undefined;
-//           if (!passwordHash) throw new Error();
-//           const idempotencyKey = Reflect.get(c.input, "idempotencyKey") as
-//             | string
-//             | undefined;
-//           if (!idempotencyKey) throw new Error();
-//           console.log(
-//             "call paddle customers create api",
-//             emailEncrypted,
-//             idempotencyKey
-//           );
-//           const customer = { id: "<paddle-customer-id>" };
-//           return {
-//             emailEncrypted,
-//             passwordHash,
-//             paddleCustomerId: customer.id,
-//           };
-//         },
-//         async onCompensate(c) {
-//           const paddleCustomerId = Reflect.get(c.output, "paddleCustomerId") as
-//             | string
-//             | undefined;
-//           if (!paddleCustomerId) throw new Error();
-//           console.log("call paddle customers update api", paddleCustomerId);
-//           return {};
-//         },
-//       },
-//       {
-//         kind: "in-tx",
-//         async onExecute(c) {
-//           const emailEncrypted = Reflect.get(c.input, "emailEncrypted") as
-//             | string
-//             | undefined;
-//           if (!emailEncrypted) throw new Error();
-//           const passwordHash = Reflect.get(c.input, "passwordHash") as
-//             | string
-//             | undefined;
-//           if (!passwordHash) throw new Error();
-//           const paddleCustomerId = Reflect.get(c.input, "paddleCustomerId") as
-//             | string
-//             | undefined;
-//           if (!paddleCustomerId) throw new Error();
-//           const emailHash = "";
-//           const result = await c.tx.query<{ id: number }>(sql`
-//             INSERT INTO users
-//               (email_encrypted, email_hash, password_hash)
-//             VALUES
-//               (${emailEncrypted}, ${emailHash}, ${passwordHash});
-//           `);
-//           return { userId: result.rows[0]?.id };
-//         },
-//         async onCompensate(c) {
-//           const userId = Reflect.get(c.output, "userId") as number | undefined;
-//           if (!userId) throw Error();
-//           await c.tx.query(sql`
-//             DELETE FROM users WHERE id = ${userId};
-//           `);
-//           return {};
-//         },
-//       },
-//     ],
-//   };
-
-//   const pool = new pg.Pool({ connectionString: process.env["DATABASE_URL"]! });
-//   await transaction(pool, async (tx) => {
-//     const uuid: string = randomUUID();
-//     const idempotencyKey: string = randomUUID();
-//     const result = await tx.query<{
-//       id: string;
-//       idempotency_key: string;
-//       input: unknown;
-//       output: unknown;
-//       created_at: number;
-//       updated_at: string;
-//     }>(sql`
-//       SELECT * FROM _monosaga_sagas WHERE idempotency_key = ${"a1acc725-46c8-411e-ab28-9b8781899331"}::uuid;
-//     `);
-//     const saga = result.rows.map(toCamel)[0];
-//     console.log(saga);
-//     await tx.query(sql`
-//       INSERT INTO _monosaga_sagas
-//         (id, idempotency_key, step_name)
-//       VALUES
-//         (${uuid}::uuid, ${idempotencyKey}::uuid, ${"test"});
-//     `);
-//   });
-// }
-
 export class SagaService {
   #db: pg.Pool;
 
@@ -188,17 +44,17 @@ export class SagaService {
         `);
         console.log(result);
       }
-      // const result = await tx.query<{
-      //   id: string;
-      //   idempotency_key: string;
-      //   input: unknown;
-      //   output: unknown;
-      //   created_at: Date;
-      //   updated_at: Date;
-      // }>(sql`
-      //   SELECT * FROM _monosaga_sagas WHERE idempotency_key = ${"a1acc725-46c8-411e-ab28-9b8781899331"}::uuid;
-      // `);
-      // console.log(result);
+      const result = await tx.query<{
+        id: string;
+        idempotency_key: string;
+        input: unknown;
+        output: unknown;
+        created_at: Date;
+        updated_at: Date;
+      }>(sql`
+        SELECT * FROM _monosaga_sagas WHERE idempotency_key = ${"a1acc725-46c8-411e-ab28-9b8781899331"}::uuid;
+      `);
+      console.log(result);
     });
     return { id: "" };
   }
@@ -240,3 +96,119 @@ export class SagaService {
 //     return definition;
 //   }
 // }
+
+type SagaOptions = {
+  a: number;
+};
+
+type SagaConfig = {
+  name: string;
+  options: SagaOptions;
+  steps: SagaStep[];
+};
+
+type Activity = { kind: "activity" };
+
+type SagaStep = TxActivities | Activity;
+
+type Saga = { kind: "saga" } & SagaConfig;
+
+function defineSaga(
+  name: string,
+  options: SagaOptions,
+  ...steps: SagaStep[]
+): Saga;
+function defineSaga(name: string, ...steps: SagaStep[]): Saga;
+function defineSaga(config: SagaConfig): Saga;
+function defineSaga(...args: unknown[]): Saga {
+  const nameOrConfig = args[0];
+  if (nameOrConfig) {
+    if (typeof nameOrConfig === "object") {
+      const config = nameOrConfig as SagaConfig;
+      return {
+        ...config,
+        kind: "saga",
+      };
+    } else if (typeof nameOrConfig === "string") {
+      const name = nameOrConfig as string;
+      const optionsOrSteps = args[1];
+      if (optionsOrSteps) {
+        if (typeof optionsOrSteps === "object") {
+          const steps = args[2];
+          if (Array.isArray(steps)) {
+            return {
+              kind: "saga",
+              name,
+              options: optionsOrSteps as SagaOptions,
+              steps: steps as SagaStep[],
+            };
+          } else {
+            throw new Error();
+          }
+        } else if (Array.isArray(optionsOrSteps)) {
+          return {
+            kind: "saga",
+            name,
+            options: { a: 1 },
+            steps: optionsOrSteps as SagaStep[],
+          };
+        } else {
+          throw new Error();
+        }
+      } else {
+        throw new Error();
+      }
+    } else {
+      throw new Error();
+    }
+  } else {
+    throw new Error();
+  }
+}
+
+type TxActivity = {
+  kind: "tx-activity";
+};
+
+function tx(...steps: TxActivity[]): TxActivities {
+  throw new Error(`${steps}`);
+}
+
+type TxActivities = { kind: "tx-activities" };
+
+function defineTxActivity(
+  name: string,
+  config: { run: () => void; compensate: () => void }
+): TxActivity {
+  throw new Error(`${name}${config}`);
+}
+
+function defineActivity(
+  name: string,
+  config: { run: () => void; compensate: () => void }
+): Activity {
+  throw new Error(`${name}${config}`);
+}
+
+const validateEmail = defineTxActivity("validateEmail", {
+  run: () => {},
+  compensate: () => {},
+});
+
+const createPaddleCustomer = defineActivity("createPaddleCustomer", {
+  run: () => {},
+  compensate: () => {},
+});
+
+const createUser = defineTxActivity("createUser", {
+  run: () => {},
+  compensate: () => {},
+});
+
+const a = defineSaga(
+  "createUser",
+  tx(validateEmail),
+  createPaddleCustomer,
+  tx(createUser)
+);
+console.log(a);
